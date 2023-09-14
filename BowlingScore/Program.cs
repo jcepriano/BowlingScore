de362d1cb37c63bfcd694d3057d6f961ba37e623
jcepriano <jamescepriano@gmail.com>
2023-09-14 13:27:30 -0700

    Add project files.

---
+++ BowlingScore/Program.cs
@@ -0,0 +1,37 @@
+﻿public class Game
+{
+    public int CurrentFrame { get; set; }
+    public int CurrentRole { get; set; }
+    public List<int> Rolls { get; set; }
+    public List<int> FrameScores { get; set; }
+
+    public Game() 
+    {
+        CurrentFrame = 1;
+        CurrentRole = 1;
+        Rolls = new List<int>();
+        FrameScores = new List<int>();
+    }
+
+    public void Roll(int pins)
+    {
+        Rolls.Add(pins);
+
+        if (pins == 10)
+        {
+            CurrentFrame++;
+            int strikeBonus = 0;
+
+            if (Rolls.Count >= (CurrentRole + 2))
+            {
+                strikeBonus = Rolls[CurrentRole] + Rolls[CurrentRole + 1];
+            }
+            else if (Rolls.Count == (CurrentRole + 1))
+            {
+                strikeBonus = Rolls[CurrentRole];
+            }
+
+            FrameScores.Add(10 + strikeBonus);
+        }
+    }
+}
